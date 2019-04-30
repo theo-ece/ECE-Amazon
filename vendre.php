88277097e381c719571f52ae4ec71eb16f9a5ef5
Lrolrolro <50113397+Lrolrolro@users.noreply.github.com>
2019-04-30 12:35:33 +0200

    Add files via upload

---
+++ vendre.php
@@ -0,0 +1,35 @@
+<?php
+	// $nom= isset($_POST["nom"])  ==> si la variable nom vaut $_Post[om]
+	// ?$_POST["nom"]		     	==> alors nom prend la valeur
+	// :""							==> sinnon prend la valeur 'vide'
+
+	$Catégorie= isset($_POST["Catégorie"])? $_POST["Catégorie"] :"";
+	$Livre= isset($_POST["Livre"])? $_POST["Livre"] :"";
+	$Musique= isset($_POST["Musique"])? $_POST["Musique"] :"";
+	$Vetement= isset($_POST["Vetement"])? $_POST["Vetement"] :"";
+	$nom= isset($_POST["nom"])? $_POST["nom"] :"";
+	$error ="";
+
+	if($Catégorie == $Livre)
+	{
+		header('Location: http://localhost/ECE-Amazon-master/index.html');
+  		exit();
+	}
+	if($Catégorie == $Musique)
+	{
+		header('Location: http://localhost/ECE-Amazon-master/index.html');
+  		exit();
+	}
+	if($Catégorie == $Sport)
+	{
+		header('Location: http://localhost/ECE-Amazon-master/index.html');
+  		exit();
+	}
+	if($Catégorie == $Vetement)
+	{
+		header('Location: http://localhost/ECE-Amazon-master/index.html');
+  		exit();
+	}
+	
+
+?>
